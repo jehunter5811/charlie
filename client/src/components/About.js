08957dbf7bee127137f52e64fad7ceafbc08d169
jhunter5811 <jhunter@learningmachine.com>
2017-12-09 10:07:38 -0600

    Styling fix on header and about page update

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -17,12 +17,10 @@ class About extends Component {
           <p>Charlie Allison surrounds himself with dying languages, fading pantheons and idle speculation. He has worn many hats: working as a chess coach, a groundskeeper, an English tutor, creative writing instructor for adults with learning disabilities, and an amateur circus acrobat.
           </p>
           <p>
-          His current headwear is that of a graduate student at Arcadia University, where he works as a graduate assistant. He helps run a writing board called <a href="http://fitsofprint.proboards.com/" target="_blank"> “Fits of Print”</a> with some other genre writers–free proofreading, critiques, support and line edits, alongside copious nerd references.
-
-          He lives in West Philadelphia. He is currently working on the draft of his second novel–after launching the first into space.
+          His current headwear is that of an amateur Mesoamerican scholar, bureaucrat and short story writer. He runs a writing board called <a href="http://fitsofprint.proboards.com/" target="_blank">“Fits of Print”</a> with some other genre writers offering free proofreading, critiques, support and line edits, alongside copious nerd references. He lives in West Philadelphia. He is currently working on the draft of his second novel–after launching the first into space.
           </p>
           <p>
-            You can find him online at ‘Fits of Print’ and on Twitter <a href="https://twitter.com/cballison421" target="_blank">@cballison421</a>.
+            He graduated with distinction from Arcadia University's Creative Writing MFA program. You can find him online at ‘Fits of Print’ and on Twitter <a href="https://twitter.com/cballison421" target="_blank">@cballison421</a>.
           </p>
         </div>
         <AddTransactionButton />
